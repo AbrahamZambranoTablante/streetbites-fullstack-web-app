86d0fd441dc84b89d2f8c2095a56a4ae453c323d
AbrahamZambranoTablante <vbrahamzt@gmail.com>
2024-07-21 17:59:23 -0400

    ray: add comments on server.js

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,11 @@
+//import app from app.js
 const app = require("./app.js");
+//configure the package dotenv so we can use process method for importing values from .env file
 require("dotenv").config();
+// assign variable PORT to hold the value from .env file
 const PORT = process.env.PORT;
 
+//call listen method for app, and its gonna listen from PORT its going to display what port its lisening on the console
 app.listen(PORT, () => {
     console.log(`LISTENING ON PORT ${PORT}`);
 });
