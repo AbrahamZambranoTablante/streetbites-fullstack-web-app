61ae4d9acaf4e981182ca8063738017de8dd385b
runquan-ray-zhou <151119924+runquan-ray-zhou@users.noreply.github.com>
2024-07-22 19:44:24 -0400

    Abraham: change double quotes to single quote in see file and update vendor  website link

--- backend/db/seed.sql
+++ backend/db/seed.sql
@@ -5,4 +5,4 @@ INSERT INTO
     vendors
     (name, phone, cuisine, address, neighborhood, borough, description, price_range, menu_photo, vendor_photo, vegan, likes)
     VALUES
-    ("Cholados & Mangos", "929-5414-0486", "Colombian", "86-01 Northern Blvd", "Jackson Heights", "Queens", "Refreshing Colombian fruity drinks!", "$10-$20", "https://instagram/choladosymangos", "https://instagram/choladosymangos", true, 500);
+    ('Cholados & Mangos', '929-5414-0486', 'Colombian', '86-01 Northern Blvd', 'Jackson Heights', 'Queens', 'Refreshing Colombian fruity drinks!', '$10-20','https://www.instagram.com/choladosymangos', 'https://www.instagram.com/choladosymangos', true, 500);
