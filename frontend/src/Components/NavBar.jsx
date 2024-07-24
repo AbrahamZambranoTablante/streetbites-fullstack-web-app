de2f93c088550779e1bbf6d8841135af985995d0
runquan-ray-zhou <151119924+runquan-ray-zhou@users.noreply.github.com>
2024-07-23 21:15:16 -0400

    Abraham: update navbar and add css

--- frontend/src/Components/NavBar.jsx
+++ frontend/src/Components/NavBar.jsx
@@ -1,7 +1,27 @@
+import { Link } from "react-router-dom"
+import "../CSS/NavBar.css"
+
 export default function NavBar () {
+
     return (
-        <>
-            <h1>THIS IS NavBar</h1>
-        </>
+        <nav>
+            <div className="navbar__links">
+                <Link to="/">
+                    <p className="navbar__links-logo">StreetBites</p>
+                </Link>
+                <Link to="/vendors/topfavorites">
+                    <p className="navbar__links-topfavorites">Top Favorites</p>
+                </Link>
+                <Link to="/vendors/cheapest">
+                    <p className="navbar__links-cheapest">Cheapest</p>
+                </Link>
+                <Link to="/vendors">
+                    <p className="navbar__links-bycuisine">Find By Cuisine</p>
+                </Link>
+                <Link to="/vendors/new">
+                    <p className="navbar__links-new">New Vendor</p>
+                </Link>
+            </div>
+        </nav>
     )
 }
