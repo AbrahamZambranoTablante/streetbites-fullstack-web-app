b28cb78ca862436497031e2dabca758be3beba56
runquan-ray-zhou <151119924+runquan-ray-zhou@users.noreply.github.com>
2024-07-23 21:21:14 -0400

    Abraham: update footer and add css

--- frontend/src/Components/Footer.jsx
+++ frontend/src/Components/Footer.jsx
@@ -1,7 +1,16 @@
+import { Link } from "react-router-dom"
+import "../CSS/Footer.css"
+
 export default function Footer () {
     return (
-        <>
-            <h1>THIS IS FOOTER</h1>
-        </>
+        <footer>
+            <Link to="https://github.com/AbrahamZambranoTablante/streetbites-fullstack-web-app">
+                <p>Source Code</p>
+            </Link>
+                <p>All Rights Reserved 2024</p>
+            <Link to="/about">
+                <p>About</p>
+            </Link>
+        </footer>
     )
 }
