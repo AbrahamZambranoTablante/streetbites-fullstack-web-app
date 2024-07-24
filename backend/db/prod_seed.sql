f2e3e021f85916f3cb69e55072afc33bb7e3d897
AbrahamZambranoTablante <vbrahamzt@gmail.com>
2024-07-24 18:21:06 -0400

    ray: add prod_schema pord_seed and dbConfig for deployment

---
+++ backend/db/prod_seed.sql
@@ -0,0 +1,17 @@
+-- connect to database
+\c vendors_prod;
+
+INSERT INTO
+    vendors
+    (name, phone, cuisine, address, neighborhood, borough, description, price_range, menu_photo, vendor_photo, vegan, likes)
+    VALUES
+    ('Cholados & Mangos', '929-514-0486', 'Colombian', '86-01 Northern Blvd', 'Jackson Heights', 'Queens', 'Refreshing Colombian fruity drinks!', '$10-20','https://www.instagram.com/choladosymangos', 'https://www.instagram.com/choladosymangos', true, 500),
+    ('Raspados Yesenia', '347-771-2137', 'Mexican', '48th St & Northern Blvd', 'Astoria', 'Queens', 'Sweets and dessert buffet to fight the summer heat!', '$10-20', 'https://www.facebook.com/raspadosyesenia/', 'https://www.facebook.com/raspadosyesenia/', true, 329),
+    ('Morocho Peruvian Fusion','347-938-5884', 'Peruvian', 'Marlborough Rd & Cortelyou Rd', 'Flatbush', 'Brooklyn', 'The best Peruvian Fusion food truck in all of New York City', '$10-20', 'https://www.instagram.com/morochonyc/', 'https://www.instagram.com/morochonyc/', true, 790),
+    ('Christos Souvlaki Stand', '646-400-7353', 'Greek', '31st St at, 23rd Ave', 'Astoria', 'Queens', 'We put the Greek in Greek Souvlaki!', '$10-20', 'https://www.yelp.com/biz/christos-souvlaki-stand-queens', 'https://www.yelp.com/biz/christos-souvlaki-stand-queens', false, 334),
+    ('Pinchos La Suquita', ' ', 'Mexican', '102-27 Roosevelt Ave', 'Corona', 'Queens', 'Every type of Mexican Barbeque available!', '$1-10', 'https://www.yelp.com/biz/pinchos-la-suquita-queens', 'https://www.yelp.com/biz/pinchos-la-suquita-queens', false, 237),
+    ('Los Chuzos Y Algo Mas', ' ', 'Colombian', '76th St & Roosevelt Ave', 'Jackson Heights', 'Queens', 'Absolute king of the Colombian kabobs!', '$1-10', 'https://www.yelp.com/biz/chuzos-cart-jackson-heights', 'https://www.yelp.com/biz/chuzos-cart-jackson-heights', false, 159),
+    ('Chestnut King', '718-581-9077', 'Chinese', '41-40 Main St', 'Flushing', 'Queens', 'Fresh Roasted Chestnut!', '$1-5', 'https://www.yelp.com/biz/chestnut-king-flushing', 'https://www.yelp.com/biz/chestnut-king-flushing', true, 47),
+    ('Cheong Fun Cart', ' ', 'Chinese', '159 Hester St', 'Chinatown', 'Manhattan', 'Very affordable Cantonese rice rolls!', '$1-5', 'https://www.yelp.com/biz/cheong-fun-cart-new-york', 'https://www.yelp.com/biz/cheong-fun-cart-new-york', false, 363),
+    ('Tamales y Elote Cart', ' ', 'Mexican', 'Elmhurst Ave & Broadway', 'Elmhurst', 'Queens', 'Homemade tamale of different varieties!', '$1-5', 'https://www.yelp.com/biz/tamales-y-elote-cart-jackson-heights', 'https://www.yelp.com/biz/tamales-y-elote-cart-jackson-heights', false, 76),
+    ('BBQ Truck', '347-379-5026', 'Chinese', '85-01 Whitney Ave', 'Elmhurst', 'Queens', 'Delicious BBQ on HOT sticks!', '$1-10', 'https://g.co/kgs/oooQaD6', 'https://g.co/kgs/oooQaD6', false, 100);
