7b19b2a4efa7391f7a6c1f4b01f8fa1dbe762003
AbrahamZambranoTablante <vbrahamzt@gmail.com>
2024-07-24 20:55:59 -0400

    ray: top favorites and cheapest frontend indexes working

--- frontend/src/Components/Vendors.jsx
+++ frontend/src/Components/Vendors.jsx
@@ -1,7 +1,32 @@
+import React from "react"
+import { useEffect, useState } from "react"
+import { useParams } from "react-router-dom";
+const API = import.meta.env.VITE_API_URL;
+import Vendor from './Vendor'
+
+
 export default function Vendors () {
+    const { selection } = useParams();
+    const [vendors, setVendors] = useState([]);
+
+    useEffect(() => {
+        fetch(`${API}/vendors/${selection}`)
+        .then(res => res.json())
+        .then(resJSON => setVendors(resJSON))
+        .catch(error => {
+            console.error(error)
+        })
+    }, [selection])
+
     return (
         <>
-            <h1>THIS IS Vendors with S</h1>
+            <div className="vendors">
+                {
+                    vendors.map(vendor => {
+                        return <Vendor key={vendor.id} vendor={vendor}/>
+                    })
+                }
+            </div>
         </>
     )
 }
